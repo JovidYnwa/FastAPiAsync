f9c82d915a269d04fb752fb66732ddf039a2180b
Jovid <jovid.9@gmail.com>
2023-03-19 15:03:50 +0500

    user Manager (registrating method is ready)

---
+++ resources/auth.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from managers.user import UserManager
+
+
+router = APIRouter(tags=["Auth"])
+
+@router.post("/register/", status_code=201)
+async def register(user_data):
+    token = UserManager.register(user_data)
+    return {"token": token}
