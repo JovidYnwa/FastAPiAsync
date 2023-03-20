d6c2e7656ac290327172f45bc98a35c4c23e819a
Jovid <jovid.9@gmail.com>
2023-03-20 16:15:42 +0500

    routs for complaints are ready

--- resources/complaints.py
+++ resources/complaints.py
@@ -1,12 +1,21 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from starlette.requests import Request
 
 from managers.auth import oauth2_scheme
 from managers.complaint import ComplaintManager
+from shemas.request.complaint import ComplaintIn
+from shemas.response.complaint import ComplaintOut
 
 router = APIRouter(tags=["Complaints"])
 
-@router.get("/complaints/", dependencies=[Depends(oauth2_scheme)])
-async def get_complaints(requst: Request):
-    user = requst.state.user
+
+@router.get("/complaints/", dependencies=[Depends(oauth2_scheme)], response_model=List[ComplaintOut])
+async def get_complaints(request: Request):
+    user = request.state.user
     return await ComplaintManager.get_comlaints(user)
+
+
+@router.post("/complaints/", dependencies=[Depends(oauth2_scheme)], response_model=ComplaintOut)
+async def create_complaints(complaint: ComplaintIn):
+    return await ComplaintManager.create_complaint(complaint.dict)
